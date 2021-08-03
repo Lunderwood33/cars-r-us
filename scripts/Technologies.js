bde21adbfa08c8e4feb3404a062fb8157ea61563
Lunderwood33 <donnie.underwood.11@gmail.com>
2021-08-03 10:47:20 -0500

    rendered technology and wheels

--- scripts/Technologies.js
+++ scripts/Technologies.js
@@ -1,1 +1,30 @@
-import { getTechnologies } from "./database.js"
+import { getTechnologies, setTechnology } from "./database.js"
+
+const technologies = getTechnologies()
+
+document.addEventListener(
+    "change",
+    (event) => {
+        if (event.target.name === "technology") {
+            setTechnology(parseInt(event.target.value))
+        }
+    }
+)
+
+export const Technologies = () => {
+    let html = "<ul>"
+
+    // Use .map() for converting objects to <li> elements
+    const listItemsArray =  technologies.map(technology => {
+        return `<li>
+            <input type="radio" name="technology" value="${technology.id}" /> ${technology.package}
+        </li>`
+    })
+
+
+    // Join all of the strings in the array into a single string
+    html += listItemsArray.join("")
+
+    html += "</ul>"
+    return html
+}
--- scripts/Wheels.js
+++ scripts/Wheels.js
@@ -1,1 +1,30 @@
-import { getWheels } from "./database.js"
+import { getWheels, setWheel } from "./database.js"
+
+const wheels = getWheels()
+
+document.addEventListener(
+    "change",
+    (event) => {
+        if (event.target.name === "wheel") {
+            setWheel(parseInt(event.target.value))
+        }
+    }
+)
+
+export const Wheels = () => {
+    let html = "<ul>"
+
+    // Use .map() for converting objects to <li> elements
+    const listItemsArray =  wheels.map(wheel => {
+        return `<li>
+            <input type="radio" name="wheel" value="${wheel.id}" /> ${wheel.size}
+        </li>`
+    })
+
+
+    // Join all of the strings in the array into a single string
+    html += listItemsArray.join("")
+
+    html += "</ul>"
+    return html
+}
