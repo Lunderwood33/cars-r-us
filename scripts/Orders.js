0d89316b62b0db1bdb0d2ce412d712f6b9cfffe4
Lunderwood33 <donnie.underwood.11@gmail.com>
2021-08-13 13:09:38 -0500

    orders now display when you click the custom order button

---
+++ scripts/Orders.js
@@ -0,0 +1,25 @@
+import { getPaints, getOrders, getInteriors, getTechnologies, getWheels } from "./database.js"
+
+
+const buildOrderListItem = (order) => {
+    return `<li>
+    Order #${order.id} cost ${order.timestamp}
+</li>`
+}
+
+export const Orders = () => {
+    /*
+        Can you explain why the state variable has to be inside
+        the component function for Orders, but not the others?
+    */
+    const orders = getOrders()
+
+    let html = "<ul>"
+
+    const listItems = orders.map(buildOrderListItem)
+
+    html += listItems.join("")
+    html += "</ul>"
+
+    return html
+}
